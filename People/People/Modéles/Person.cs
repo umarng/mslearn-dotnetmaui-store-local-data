78fdad08e9b0083135f3f144a6dbc6c493194c98
ASUS <ASUS@PC-FORM-04>
2024-05-06 19:14:53 +0000

    mon premier commit

---
+++ People/People/Modéles/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace People.Modéles;
+[Table("people")]
+public class Person
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
+}
